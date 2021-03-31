0336fcdf4db3c170c3947b20797abe27add82fbf
mhcheng1 <80797236+mhcheng1@users.noreply.github.com>
2021-03-30 20:36:23 -0400

    Upload Unity

---
+++ Unity_Assets/3D Obstacle/Button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+   public void QuitMyGame ()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
---
+++ Unity_Assets/3D Obstacle/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement Movement;
+    public GameManager gameManager;
+
+    // Update is called once per frame
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+        
+    }
+}
